e9ab654288ecef61a533e44a4e6981aa6dd8c5e2
사ìSeoHoejeong <parang@naver.com>
2025-04-30 11:48:19 +0900

    ã회원가입,ê¸, 글쓰기 DTO 전환 완료

--- src/main/java/shop/mtcoding/blog/user/UserResponse.java
+++ src/main/java/shop/mtcoding/blog/user/UserResponse.java
@@ -1,5 +1,22 @@
 package shop.mtcoding.blog.user;
 
+import lombok.Data;
+
 public class UserResponse {
 
+    // 규칙2 : DTO에 민감정보 빼기(패스워드 같은거), 날짜는 String으로 (날짜 공부하기 전까지)
+    @Data
+    public static class DTO {
+        private Integer id;
+        private String username;
+        private String email;
+        private String createdAt;
+
+        public DTO(User user) {
+            this.id = user.getId();
+            this.username = user.getUsername();
+            this.email = user.getEmail();
+            this.createdAt = user.getCreatedAt().toString(); // 문자열로 바꿀거니까 toString() 필요
+        }
+    }
 }
