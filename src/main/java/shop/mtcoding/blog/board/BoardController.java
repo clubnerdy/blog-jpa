39959859bd82cfa1e9d41e840619f9f38e508d01
사ìSeoHoejeong <parang@naver.com>
2025-05-12 09:35:55 +0900

    코드리팩토링

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,10 +1,9 @@
 package shop.mtcoding.blog.board;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.blog._core.util.Resp;
@@ -12,41 +11,40 @@ import shop.mtcoding.blog.user.User;
 
 @CrossOrigin
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class BoardController {
     private final BoardService boardService;
     private final HttpSession session;
 
-    //update board_tb set title = ?, content = ?, is_pubic = ? where id = ?
+    // update board_tb set title = ?, content = ?, is_public = ? where id = ?
     @PutMapping("/s/api/board/{id}")
-    public @ResponseBody Resp<?> update(@PathVariable("id") Integer id, @Valid @RequestBody BoardRequest.UpdateDTO reqDTO, Errors errors) {
+    public ResponseEntity<?> update(@PathVariable("id") Integer id, @Valid @RequestBody BoardRequest.UpdateDTO reqDTO, Errors errors) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         BoardResponse.DTO respDTO = boardService.글수정하기(reqDTO, id, sessionUser.getId());
 
         return Resp.ok(respDTO);
     }
 
-    @GetMapping("/api/board/{id}") //update-form
-    public @ResponseBody Resp<?> setBoardOne(@PathVariable("id") int id) {
+    @GetMapping("/api/board/{id}")
+    public ResponseEntity<?> getBoardOne(@PathVariable("id") int id) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         BoardResponse.DTO respDTO = boardService.글보기(id, sessionUser.getId());
         return Resp.ok(respDTO);
     }
 
     @GetMapping("/api/board/{id}/detail")
-    public @ResponseBody Resp<?> getBoardDetail(@PathVariable("id") Integer id) {
+    public ResponseEntity<?> getBoardDetail(@PathVariable("id") Integer id) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         Integer sessionUserId = (sessionUser == null ? null : sessionUser.getId());
-        BoardResponse.DetailDTO respDTO = boardService.글상세보기(id, sessionUserId);
-        return Resp.ok(respDTO);
+        BoardResponse.DetailDTO detailDTO = boardService.글상세보기(id, sessionUserId);
+        return Resp.ok(detailDTO);
     }
 
     // localhost:8080?page=0
     // localhost:8080
-    @GetMapping({"/", "/api/board"}) // 일반적으로 index 페이지는 두 가지 주소를 사용함
-    public @ResponseBody Resp<?> list(HttpServletRequest request,
-                                      @RequestParam(required = false, value = "page", defaultValue = "0") Integer page,
-                                      @RequestParam(required = false, value = "keyword", defaultValue = "") String keyword) {
+    @GetMapping({"/", "/api/board"})
+    public ResponseEntity<?> list(@RequestParam(required = false, value = "page", defaultValue = "0") Integer page,
+                                  @RequestParam(required = false, value = "keyword", defaultValue = "") String keyword) {
         User sessionUser = (User) session.getAttribute("sessionUser");
 
         BoardResponse.ListDTO respDTO;
@@ -59,8 +57,8 @@ public class BoardController {
         return Resp.ok(respDTO);
     }
 
-    @PostMapping("/s/api/board") // save
-    public @ResponseBody Resp<?> save(@Valid @RequestBody BoardRequest.SaveDTO reqDTO, Errors errors) {
+    @PostMapping("/s/api/board")
+    public ResponseEntity<?> save(@Valid @RequestBody BoardRequest.SaveDTO reqDTO, Errors errors) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         BoardResponse.DTO respDTO = boardService.글쓰기(reqDTO, sessionUser);
         return Resp.ok(respDTO);
